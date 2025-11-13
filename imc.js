3cc738473575cc46744a9c68a5d05523e45d5cd2
JoaoPFerC <jpfernandesdecampos2@gmail.com>
2025-11-13 15:59:30 -0300

    Exercicio IMC

---
+++ imc.js
@@ -0,0 +1,12 @@
+let peso = 62
+let altura = 165
+let nome = "Jp"
+let IMC = ( altura*altura) / 2
+
+if( IMC <=18,5){
+    console.log(`O paciente ${nome} está abaixo do peso ${IMC.toFixed(1)}`)
+}else if (IMC >= 18,5){
+    console.log(`O paciente ${nome} está com o peso normal ${IMC.toFixed(1)}`)
+}else { (IMC = 29,9)
+      console.log(`O paciente ${nome} está abaixo do peso ${IMC.toFixed(1)}`)
+}
