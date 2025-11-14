c40cb189e796c3a56274ccd4955a9bce20f575bd
JoaoPFerC <jpfernandesdecampos2@gmail.com>
2025-11-14 15:10:56 -0300

    viagem

--- viagem.js
+++ viagem.js
@@ -1,4 +1,8 @@
 let velocidadeMedia = 100
 let tempo = 10
 let consumoMedio = 12
+//let gasolina = 5.89
+//let etanol = 3.89
 
+let distancia = (velocidadeMedia * tempo)
+console.log(`A distância será de ${distancia.toFixed(3)}`)
